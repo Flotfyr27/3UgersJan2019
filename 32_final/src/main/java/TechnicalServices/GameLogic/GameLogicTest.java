e9bed232044ee849f4b58c3b958f72021b8322d8
Gustav Vang Klinker <gulle139@hotmail.com>
2019-01-09 16:17:45 +0100

    Test of GameLogic methods propertyPrice and chanceCardValues; working

--- 32_final/src/main/java/TechnicalServices/GameLogic/GameLogicTest.java
+++ 32_final/src/main/java/TechnicalServices/GameLogic/GameLogicTest.java
@@ -3,13 +3,14 @@ package main.java.TechnicalServices.GameLogic;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameLogicTest {
-    GameLogic gl;
+    GameLogic gl = new GameLogic();
     @org.junit.jupiter.api.Test
     void propertyPrice() {
-        assertEquals(gl.propertyPrice(10),6000 );
+        assertEquals(gl.propertyPrice(33),6000 );
     }
 
     @org.junit.jupiter.api.Test
     void chanceCardValue() {
+        assertEquals(gl.chanceCardValue(18), 1000);
     }
 }
