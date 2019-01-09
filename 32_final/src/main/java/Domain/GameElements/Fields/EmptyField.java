f3d42c38cacd793491126031b28ba62754818943
Jens.Agersgaard <alex.norgaard@gmail.com>
2019-01-09 14:09:36 +0100

    Created emptyField and TaxField. TaxField not done.

---
+++ 32_final/src/main/java/Domain/GameElements/Fields/EmptyField.java
@@ -0,0 +1,10 @@
+package Domain.GameElements.Fields;
+
+import java.awt.*;
+
+public class EmptyField extends Field {
+
+    public EmptyField (String name, String subtext, Color bgColour){
+        super(name, subtext, bgColour);
+    }
+}
