840d77b4d32a75f98dca6727954c62e4e2f16d8e
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-09 11:04:07 +0100

    Added comment for initialization of field[0] (start).

--- 32_final/src/main/java/Domain/GameElements/Board.java
+++ 32_final/src/main/java/Domain/GameElements/Board.java
@@ -8,6 +8,6 @@ private Field[] fields;
 
     private void setFields(){
     fields = new Field[40];
-    fields[0] = (new (EmptyField("START", "Hver gang de passerer START, modtag kr. 4000")));
+   //fields[0] = (new (EmptyField("START", "Hver gang de passerer START, modtag kr. 4000")));
     }
 }
