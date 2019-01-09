8cd8ae27488e4f8657091489117f075f7f21e6c6
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-09 13:41:34 +0100

    Added CompanyField.java

---
+++ 32_final/src/main/java/Domain/GameElements/Fields/Ownable/CompanyField.java
@@ -0,0 +1,19 @@
+package Domain.GameElements.Fields.Ownable;
+
+import java.awt.*;
+
+public class CompanyField extends OwnableField {
+    public CompanyField(String name, String subtext, Color bgColour, Player owner, int price, int rent){
+        super(name, subtext, bgColour, owner, price, rent);
+    }
+
+    @Override
+    public void buy() {
+
+    }
+
+    @Override
+    public void sell() {
+
+    }
+}
