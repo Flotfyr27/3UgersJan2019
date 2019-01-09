88a6a27c6ef84ef799f9622f3da92373584f5904
MLK <s185108@student.dtu.dk>
2019-01-09 16:33:17 +0100

    created TaxCard class

---
+++ 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/TaxCard.java
@@ -0,0 +1,12 @@
+package Domain.GameElements.Entities.Chancecard;
+import Domain.GameElements.Entities.Player;
+import Domain.GameElements.Fields.Ownable.*;
+
+
+public class TaxCard {
+    private int amount;
+
+    public void action(Player player,Player  ); /**Kan ikke udfyldes før Player-klassen har en owned fields array er lavet**/
+
+
+}
