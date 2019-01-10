080e5df781e21bfcc8819d8d31ee7ee5e1e6dd75
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-10 12:33:30 +0100

    Added player init to board.java

--- 32_final/src/main/java/Domain/GameElements/Board.java
+++ 32_final/src/main/java/Domain/GameElements/Board.java
@@ -1,11 +1,22 @@
 package Domain.GameElements;
 
+import Domain.GameElements.Entities.Player;
 import Domain.GameElements.Fields.Field;
 
 public class Board {
 private Field[] fields;
+private Player[] players;
 
+public Board(int numberOfPlayers){
+    initPlayers(numberOfPlayers);
+}
 
+    private void initPlayers(int numberOfPlayers){
+        for(int i = 0; i < numberOfPlayers; i++){
+            players[i] = new Player("Player" + (i+1));
+            players[i].getAccount().changeScore(30000);
+        }
+    }
     private void setFields(){
     fields = new Field[40];
    //fields[0] = (new (EmptyField("START", "Hver gang de passerer START, modtag kr. 4000")));
