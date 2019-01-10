504e137f991bf6e8c16ce179c44e872f5dd7c4ac
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-10 14:48:44 +0100

    Added ProptertyFieldTest.java in new branch, fieldTest

---
+++ 32_final/src/test/java/Domain/GameElements/Fields/Ownable/PropertyFieldTest.java
@@ -0,0 +1,36 @@
+package Domain.GameElements.Fields.Ownable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PropertyFieldTest {
+
+    @Test
+    void getHouses() {
+    }
+
+    @Test
+    void getHotel() {
+    }
+
+    @Test
+    void getWorth() {
+    }
+
+    @Test
+    void sell() {
+    }
+
+    @Test
+    void buy() {
+    }
+
+    @Test
+    void pawn() {
+    }
+
+    @Test
+    void payRent() {
+    }
+}
