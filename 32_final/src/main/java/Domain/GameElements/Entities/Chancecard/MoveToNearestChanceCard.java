c1fe5964581e77816c45e24d78848f960f021063
Mads Møller Jensen <mads1606@gmail.com>
2019-01-10 12:45:45 +0100

    Wrote move to nearest class

--- 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/MoveToNearestChanceCard.java
+++ 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/MoveToNearestChanceCard.java
@@ -3,17 +3,33 @@ package Domain.GameElements.Entities.Chancecard;
 import Domain.GameElements.Entities.Player;
 import Domain.GameElements.Fields.Field;
 
-public class MoveToNearestChanceCard extends ChanceCard {
+public class MoveToNearestChanceCard extends MoveToChanceCard {
 
-    //TODO find ud af om der kun er mulighed for at skulle flytte til type eller
-    // om der er flere af samme type der er forskellige move to destinationer
+    private Class<Field> type;
+    private Field[] fields;
 
-    public MoveToNearestChanceCard(String description){
-        super(description);
+    public MoveToNearestChanceCard(Class<Field> type, Field[] fields, String description){
+        super(0, description);
+        this.type = type;
+        this.fields = fields;
     }
 
-    public void action(Player player) {
-
+    public void action(Player p) {
+        for (int i = p.getPos(); i < fields.length; i++) {
+            if (fields[i].getClass().equals(type)) {
+                super.value = i;
+                super.action(p);
+                return;
+            }
+        }
+        for (int i = 0; i < p.getPos(); i++) {
+            if (fields[i].getClass().equals(type)) {
+                super.value = i;
+                super.action(p);
+                return;
+            }
+        }
+        throw new RuntimeException("No Field of the specified type found");
     }
 
 }
