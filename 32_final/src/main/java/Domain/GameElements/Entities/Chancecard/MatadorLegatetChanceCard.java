df91a109bf715cc6f2c5abd62fe068f75aa0e38e
MLK <s185108@student.dtu.dk>
2019-01-10 13:54:40 +0100

    added java doc to MatadorLegateChanceCard

--- 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/MatadorLegatetChanceCard.java
+++ 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/MatadorLegatetChanceCard.java
@@ -15,7 +15,11 @@ public class MatadorLegatetChanceCard extends TransactionCard {
         super(amount, description);
     }
 
-
+    /**
+     * gives the player a large sum of money if they currently own
+     * less than 15000 worth of buildings and money
+     * @param player
+     */
     public void action(Player player) {
         int ownableWorth = 0;
         for (OwnableField field : player.getOwnedFields())
