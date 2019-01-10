e67b5c5f8ad4ab2e90a24267af7d46bd4f6f4826
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-10 10:54:24 +0100

    Added javadoc for PropertyField.java

--- 32_final/src/main/java/Domain/GameElements/Fields/Ownable/PropertyField.java
+++ 32_final/src/main/java/Domain/GameElements/Fields/Ownable/PropertyField.java
@@ -4,6 +4,16 @@ import java.awt.*;
 import Domain.GameElements.Entities.Player;
 
 public class PropertyField extends OwnableField{
+    /**
+     * Constructor for PropertyField
+     * @param name Name of the field
+     * @param subtext Subtext for the field
+     * @param bgColour background colour for the field
+     * @param owner Owner of the field
+     * @param price Price of the field
+     * @param rent Rent of the field
+     * @param housePrice Price per house on the field
+     */
     public PropertyField(String name, String subtext, Color bgColour, Player owner, int price, int rent, int housePrice){
         super(name, subtext, bgColour, owner, price, rent);
         this.housePrice = housePrice;
@@ -12,13 +22,26 @@ public class PropertyField extends OwnableField{
     private int hotelPrice = housePrice*5;
     private boolean hasHotel = false;
 
+    /**
+     * Method retrieves number of houses on the field
+     * @return integer value of number of houses
+     */
     public int getHouses(){
         return numberOfHouses;
     }
+
+    /**
+     * Method returns boolean value depending on the presence of a hotel
+     * @return True/false if field has a hotel
+     */
     public boolean getHotel(){
         return hasHotel;
     }
 
+    /**
+     * Method returns the total value of a property, which includes houses and hotels.
+     * @return Integer value of the total property value.
+     */
     @Override
     public int getWorth() {
         int worth = super.getWorth()+(numberOfHouses*housePrice);
