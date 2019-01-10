78619f0de7a19f05b64157ea7f1de26aefa72b99
Mads Møller Jensen <mads1606@gmail.com>
2019-01-10 12:51:57 +0100

    added java doc to MoveToNearest

--- 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/MoveToNearestChanceCard.java
+++ 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/MoveToNearestChanceCard.java
@@ -7,13 +7,44 @@ public class MoveToNearestChanceCard extends MoveToChanceCard {
 
     private Class<Field> type;
     private Field[] fields;
+    private boolean payDouble;
 
+    /**
+     * Constructor
+     *
+     * @param type
+     * @param fields
+     * @param description
+     */
     public MoveToNearestChanceCard(Class<Field> type, Field[] fields, String description){
         super(0, description);
         this.type = type;
         this.fields = fields;
+        payDouble = false;
     }
 
+    /**
+     * Constructor
+     *
+     * @param type
+     * @param fields
+     * @param payDouble
+     * @param description
+     */
+    public MoveToNearestChanceCard(Class<Field> type, Field[] fields, boolean payDouble, String description){
+        super(0, description);
+        this.type = type;
+        this.fields = fields;
+        this.payDouble = payDouble;
+    }
+
+    /**
+     * moves the player to the closest field of a specified class.
+     *
+     * @param p
+     *
+     * TODO implement the payDouble feature
+     */
     public void action(Player p) {
         for (int i = p.getPos(); i < fields.length; i++) {
             if (fields[i].getClass().equals(type)) {
