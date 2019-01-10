7fdd4b1941ab580e2c790893ae7891e4a4600105
Jens.Agersgaard <alex.norgaard@gmail.com>
2019-01-10 14:37:43 +0100

    Created the JailorField *Not tested*

--- 32_final/src/main/java/Domain/GameElements/Fields/JailorField.java
+++ 32_final/src/main/java/Domain/GameElements/Fields/JailorField.java
@@ -1,4 +1,42 @@
 package Domain.GameElements.Fields;
 
-public class JailorField {
+import Domain.GameElements.Entities.Player;
+
+import java.awt.*;
+
+public class JailorField extends Field {
+    /**
+     *
+     * @param name
+     * @param subtext
+     * @param bgColour
+     */
+    public JailorField(String name, String subtext, Color bgColour){
+        super (name, subtext, bgColour);
+    }
+
+    /**
+     * Moves the player to jail, and sets "Rounds in jail" to 0
+     * @param player
+     * @param players An array of all players
+     * @param fields An array of all fields
+     */
+    public void landOnAction(Player player, Player[] players, Field[] fields){
+/*
+ * If the player has a "get out of jail free" card, this removes the card in a method in players. Then next player.
+ */
+        if(player.jailCards >= 1){
+
+            player.removeJailCards();
+
+        }else{
+            player.setPos(10);
+
+            /* For use with jailController
+            player.jail = 0;
+            */
+        }
+
+
+    }
 }
