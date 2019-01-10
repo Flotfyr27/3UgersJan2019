4ccbe83ee391de534681a389bf1cf843e70d3c74
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-10 13:03:53 +0100

    Added first five fields of the board to Board.java

--- 32_final/src/main/java/Domain/GameElements/Board.java
+++ 32_final/src/main/java/Domain/GameElements/Board.java
@@ -1,7 +1,13 @@
 package Domain.GameElements;
 
 import Domain.GameElements.Entities.Player;
+import Domain.GameElements.Fields.ChanceField.ChanceField;
+import Domain.GameElements.Fields.EmptyField;
 import Domain.GameElements.Fields.Field;
+import Domain.GameElements.Fields.Ownable.PropertyField;
+import Domain.GameElements.Fields.TaxField;
+
+import java.awt.*;
 
 public class Board {
 private Field[] fields;
@@ -19,6 +25,10 @@ public Board(int numberOfPlayers){
     }
     private void setFields(){
     fields = new Field[40];
-   //fields[0] = (new (EmptyField("START", "Hver gang de passerer START, modtag kr. 4000")));
+    fields[0] = (new EmptyField("START", "Hver gang de passerer START, modtag kr. 4000", Color.RED));
+    fields[1] = (new PropertyField("Rødovrevej", "kr. 1.200", Color.CYAN, 1200, 1000));
+    fields[2] = (new ChanceField("Prøv lykken", "?", Color.BLACK));
+    fields[3] = (new PropertyField("Hvidovrevej", "kr. 1.200", Color.CYAN, 1200, 1000));
+    fields[4] = (new TaxField("Skat", "Betal indkomstskat: 10% eller kr. 4.000", Color.GREEN, ))
     }
 }
