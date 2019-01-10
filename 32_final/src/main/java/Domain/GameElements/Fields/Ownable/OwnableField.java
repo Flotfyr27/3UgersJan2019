754990634d5753bbcc74320c9d21500cd91a98cc
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-10 10:46:59 +0100

    Added javadoc for OwnableField.java

--- 32_final/src/main/java/Domain/GameElements/Fields/Ownable/OwnableField.java
+++ 32_final/src/main/java/Domain/GameElements/Fields/Ownable/OwnableField.java
@@ -8,12 +8,26 @@ public abstract class OwnableField extends Field {
     private Player owner;
     private int price, rent;
 
+    /**
+     * Constructor for all ownable fields
+     * @param name The name of the field
+     * @param subtext The fields subtext
+     * @param bgColour The background colour of the field
+     * @param owner The owner of the field
+     * @param price The price of the field
+     * @param rent The rent of the field
+     */
     public OwnableField(String name, String subtext, Color bgColour, Player owner, int price, int rent){
         super(name, subtext, bgColour);
         this.owner = owner;
         this.price = price;
         this.rent = rent;
     }
+
+    /**
+     * Returns the value of a field
+     * @return The total value of a field
+     */
     public int getWorth(){
         int worth = price;
         return worth;
