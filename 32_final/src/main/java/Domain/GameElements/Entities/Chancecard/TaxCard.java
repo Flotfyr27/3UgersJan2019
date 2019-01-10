c9548125fbad0aecfae516efa504feca05515b37
Mads Møller Jensen <mads1606@gmail.com>
2019-01-10 10:09:57 +0100

    wrote the Action() method for TaxCard;

--- 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/TaxCard.java
+++ 32_final/src/main/java/Domain/GameElements/Entities/Chancecard/TaxCard.java
@@ -1,23 +1,28 @@
 package Domain.GameElements.Entities.Chancecard;
 import Domain.GameElements.Entities.Player;
 import Domain.GameElements.Fields.Ownable.*;
-import UI.GUI.*;
 
 
 public class TaxCard extends ChanceCard{
     private int housePrice;
     private int hotelPrice;
-    private GuiHandler gui;
 
     public TaxCard(int housePrice, int hotelPrice, String description){
         super(description);
         this.housePrice = housePrice;
         this.hotelPrice = hotelPrice;
-        gui = GuiHandler.getInstance();
     }
 
     public void action(Player p){
+        int sum = 0;
 
+        for (OwnableField f : p.getOwnedFields()){
+            if (f.getClass().equals(PropertyField.class)) {
+                PropertyField pf = (PropertyField) f;
+                sum += housePrice * pf.getHouses();
+                sum += hotelPrice * pf.getHotels();
+            }
+        }
     }
 
 
