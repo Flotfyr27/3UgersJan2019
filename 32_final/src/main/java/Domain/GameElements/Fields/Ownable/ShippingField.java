ef47da438fff51544848fa61dd8e6eee5e3ddfbf
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-10 10:49:51 +0100

    Added javadoc for ShippingField.java

--- 32_final/src/main/java/Domain/GameElements/Fields/Ownable/ShippingField.java
+++ 32_final/src/main/java/Domain/GameElements/Fields/Ownable/ShippingField.java
@@ -4,6 +4,15 @@ import java.awt.*;
 import Domain.GameElements.Entities.Player;
 
 public class ShippingField extends OwnableField {
+    /**
+     * Constructor for the shippingfields
+     * @param name Name of the field
+     * @param subtext Subtext for the field
+     * @param bgColour Background colour of the field
+     * @param owner Owner of the field
+     * @param price Price of the field
+     * @param rent Rent of the field
+     */
     public ShippingField(String name, String subtext, Color bgColour, Player owner, int price, int rent){
         super(name, subtext, bgColour, owner, price, rent);
     }
