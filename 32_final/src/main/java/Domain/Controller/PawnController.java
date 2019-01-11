dcded5aadbf14486d9b0f78a84d54c8a70086e7e
MLK <s185108@student.dtu.dk>
2019-01-11 14:33:07 +0100

    Wrote in PawnController class

--- 32_final/src/main/java/Domain/Controller/PawnController.java
+++ 32_final/src/main/java/Domain/Controller/PawnController.java
@@ -3,32 +3,63 @@ package Domain.Controller;
 import Domain.GameElements.Board;
 import Domain.GameElements.Entities.Player;
 import Domain.GameElements.Fields.Ownable.OwnableField;
+import Domain.GameElements.Fields.Ownable.PropertyField;
+import Domain.GameElements.Fields.Field;
+import Domain.GameElements.Entities.Account;
 
 public class PawnController {
 
     private int pawnValue;
     private boolean fieldIsEmpty;
-    Player player = new Player("Pawner");
+
+    Player player = new Player();
+    PropertyField propertyField = new PropertyField();
+
+    Account account = new Account();
+    OwnableField ownableField;
 
 
 
 
-    private void pawnProperty() {
-        player.getOwnedFields();
-        if (fieldIsEmpty = true){
-            int ownableWorth = 0;
-            for (OwnableField field : player.getOwnedFields()) {
-                ownableWorth = field.getWorth() - field.getPrice();
+    private boolean FieldIsEmpty() {
+        this.fieldIsEmpty = fieldIsEmpty;
+        if (propertyField.getHouses() == 0 || propertyField.getHotel()) {
+            fieldIsEmpty = true;
+        } else {
+            fieldIsEmpty = false;
+        }
+        return false;
+    }
+
+        private void pawnProperty(){
+
+            if (fieldIsEmpty == true) {
+                pawnValue = ownableField.getPrice() / 2;
+
+                account.changeScore(-pawnValue);
 
             }
+            else{
+                int numberOfHouses = propertyField.getHouses();
+                propertyField.removeHouse(numberOfHouses);
+            }
+        }
+        /**if (fieldIsEmpty == true){
+            int buildingsWorth = 0;
+            for (OwnableField field : player.getOwnedFields()) {
+
+                buildingsWorth = field.getWorth() - field.getPrice();
 
 
+
+            }
+            int numberOfHouses = propertyField.getHouses();
+            propertyField.removeHouse(numberOfHouses);
         }
         else {
-
+        */
         }
     }
 
 
-    }
 
