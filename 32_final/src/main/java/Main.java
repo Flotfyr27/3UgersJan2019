a96a3259cb80bb4ea858542f1a213f737ad411c8
Mads Møller Jensen <mads1606@gmail.com>
2019-01-11 14:32:41 +0100

    started writing psvm - note finished

--- 32_final/src/main/java/Main.java
+++ 32_final/src/main/java/Main.java
@@ -1,9 +1,11 @@
 import Domain.Controller.MainController;
+import Domain.GameElements.Board;
 import UI.GUI.GuiHandler;
 
 public class Main {
     public static void main(String[] args) {
         GuiHandler guiHandler = GuiHandler.getInstance();
+        Board board;
 
 
         guiHandler.getNumberOfPlayers(3, 6);
