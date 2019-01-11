b893a6a90a53820f8c3bdf665ae7ffdd7d3c3570
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-11 16:05:02 +0100

    Added tradeWithPlayer()-method to TradeController.java

--- 32_final/src/main/java/Domain/Controller/TradeController.java
+++ 32_final/src/main/java/Domain/Controller/TradeController.java
@@ -1,7 +1,14 @@
 package Domain.Controller;
 
-public class TradeController {
-    public TradeController(){
+import UI.GUI.GuiHandler;
 
+public class TradeController {
+    private GuiHandler guiHandler;
+    public TradeController(){
+        guiHandler = GuiHandler.getInstance();
+    }
+
+    public void tradeWithPlayer(){
+        //guiHandler.makeButtons("Select a player to trade with", )
     }
 }
