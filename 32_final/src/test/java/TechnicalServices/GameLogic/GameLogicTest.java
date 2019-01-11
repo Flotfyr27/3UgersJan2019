ac5f1348828ea35b44a8a0445a48ea9b710d090b
Gustav Vang Klinker <gulle139@hotmail.com>
2019-01-11 10:41:59 +0100

    GameLogicTest fixed

--- 32_final/src/test/java/TechnicalServices/GameLogic/GameLogicTest.java
+++ 32_final/src/test/java/TechnicalServices/GameLogic/GameLogicTest.java
@@ -6,14 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameLogicTest {
     GameLogic gl = new GameLogic();
+    Values val = new Values();
     @Test
     void propertyPrice() {
-        assertEquals(gl.propertyPrice(33),6000 );
+        assertEquals(val.propertyPrice(33),6000 );
     }
 
     @Test
     void chanceCardValue() {
-        assertEquals(gl.chanceCardValue(18), 1000);
+        assertEquals(val.chanceCardValue(18), 1000);
     }
 
 }
