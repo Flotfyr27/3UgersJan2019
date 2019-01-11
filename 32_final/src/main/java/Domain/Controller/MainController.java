d4d98f4518009ecca939a4a8beb385d2e086d666
Mads Møller Jensen <mads1606@gmail.com>
2019-01-11 10:13:52 +0100

    wrote half the runCase method

--- 32_final/src/main/java/Domain/Controller/MainController.java
+++ 32_final/src/main/java/Domain/Controller/MainController.java
@@ -1,4 +1,43 @@
 package Domain.Controller;
 
+import Domain.GameElements.Entities.Player;
+import TechnicalServices.GameLogic.GameLogic;
+import UI.GUI.GuiHandler;
+
+
 public class MainController {
+
+    protected GuiHandler guiHandler;
+    protected Player[] players;
+    protected int currentPlayerNum;
+    protected Player currentPlayer;
+    private MoveController moveCon;
+    private PrisonController prisonCon;
+    private PawnController pawnCon;
+    private TradeController tradeCon;
+
+    public void runCase(){
+        while (!GameLogic.lastManStanding()){
+            currentPlayer = players[currentPlayerNum];
+            do{
+                String choice;
+                if (currentPlayer.getIsJaieled) {
+                    choice = guiHandler.makeButtons("vælg en handling",
+                            "Slip fri", "Handel", "Pantsætning");
+                    if (choice.equalsIgnoreCase("Slip fri"))
+                        prisonCon.runCase();
+                    if (choice.equalsIgnoreCase("Handel"))
+                        tradeCon.runCase();
+                    if (choice.equalsIgnoreCase("Pantsætning"))
+                        pawnCon.runCase();
+                } else {
+
+                }
+
+            }while (currentPlayer.getIsActive());
+
+            currentPlayerNum++;
+        }
+    }
+
 }
---
+++ 32_final/src/main/java/Main.java
@@ -0,0 +1,7 @@
+import UI.GUI.GuiHandler;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
