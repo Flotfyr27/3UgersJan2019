6a2b669e400a0147e025d30031682deb8b6cab43
Jens.Agersgaard <alex.norgaard@gmail.com>
2019-01-11 16:49:31 +0100

    Merge branch 'development' into JailDevelopment
    # Conflicts:
    #	32_final/src/main/java/Domain/GameElements/Fields/JailorField.java
    *resolved conflicts"
    
    Finished JailController

--- 32_final/src/main/java/Domain/Controller/JailController.java
+++ 32_final/src/main/java/Domain/Controller/JailController.java
@@ -1,34 +1,81 @@
 package Domain.Controller;
 
+import Domain.GameElements.Entities.DiceTray;
 import Domain.GameElements.Entities.Player;
 import UI.GUI.GuiHandler;
-
 
 public class JailController {
     private GuiHandler guiHandler = GuiHandler.getInstance();
     private int timeInJail;
-    DiceTray
+    private DiceTray diceTray;
 
     Player player = new Player("playerJailed");
 
-    private JailController(){
+    private static JailController instance;
 
+    /**
+     * Making the JailController a singleton
+     * @return
+     */
+    public static JailController getInstance(){
+        if(instance==null){
+            instance = new JailController();
+            return instance;
+        }else{
+            return instance;
+        }
     }
 
-    public void jailOptions() {
+    /**
+     * Constructor
+     */
+    private JailController(){
+        diceTray = new DiceTray();
+    }
+
+    /**
+     * RunCase in which the player chooses between two options for getting out of jail.
+     * @param player
+     */
+    public void runCase(Player player) {
         timeInJail = 0;
 
         do {
-            player.setJailTime(++timeInJail);
-            String buttons = guiHandler.makeButtons("Du er i fængsel. hvad vil du foretage dig?", "Betal kr.1.000, og slå terninger", "Slå terninger");
+            player.setJailTime(++timeInJail); //adds 1 to timeInJail. After 3 rounds the player is forced to pay bail to get out.
+            String buttons = guiHandler.makeButtons("Du er i fængsel. hvad vil du foretage dig?", "Betal kr.1.000, og ryk ud af fængsel", "Slå terninger");
 
-            if(buttons.equals("Betal kr.1.000, og slå terninger")){
+            //Choice of making bail and throwing the dice
+            if(buttons.equals("Betal kr.1.000, og ryk ud af fængsel")) {
                 player.getAccount().changeScore(-1000);
-
-
+                moveController; //The dice are thrown in the moveController
+                player.setJailTime(-1);
+                return;
             }
+                //Throwing dice to get double values. If both dice are the same, the player gets out of jail.
+                else if(buttons.equals("Slå terninger")){
+                    diceTray.Roll();
+
+                    if(diceTray.IsDoubleValue()) {
+                        moveController; //The dice are thrown in the moveController
+                        player.setJailTime(-1);
+                        return;
+                    }else if(!diceTray.IsDoubleValue()){
+
+                        guiHandler.msgInMiddle("Du slog desværre ikke dobbelt. Bedre held næste gang");
+                        return;
+
+                    }
+                }
+
+            }while(player.getJailTime()<3);
+
+        //The player is forced to make bail
+        if(player.getJailTime()==3){
+            player.getAccount().changeScore(-1000);
+            moveController; //The dice are thrown in the moveController
+            player.setJailTime(-1);
         }
-        while(player.getJailTime()<3);
+
     }
 
 }
