e107e436a9869bb96d3f2124a11992d2a45f2c22
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-14 12:16:32 +0100

    Added initAuction() and getPlayerIndex() to AuctionController.java in new branch auctionDevelopment

---
+++ 32_final/src/main/java/Domain/Controller/AuctionController.java
@@ -0,0 +1,9 @@
+package Domain.Controller;
+
+import Domain.GameElements.Entities.Player;
+
+public class AuctionController {
+
+    public void initAuction(){}
+    private int getPlayerIndex(Player player){return 0;}
+}
