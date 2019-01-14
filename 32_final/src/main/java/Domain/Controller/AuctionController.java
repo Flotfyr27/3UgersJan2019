96d7b8027f1724a0d1041453f111548e9666c888
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-14 13:54:17 +0100

    Added getPlayerIndex()-method in AuctionController

--- 32_final/src/main/java/Domain/Controller/AuctionController.java
+++ 32_final/src/main/java/Domain/Controller/AuctionController.java
@@ -1,9 +1,23 @@
 package Domain.Controller;
 
+import Domain.GameElements.Board;
 import Domain.GameElements.Entities.Player;
 
 public class AuctionController {
+    Board board;
+    public AuctionController(){
+        board = Board.getInstance();
+    }
 
     public void initAuction(){}
-    private int getPlayerIndex(Player player){return 0;}
+    private int getPlayerIndex(Player player){
+        int index = 0;
+        for(int n = 0; n < board.getPlayers().length; n++){
+            if(board.getPlayerAtIndex(n).equals(player)){
+                index = n;
+                break;
+            }
+        }
+        return index;
+    }
 }
