e80e5a9b3bb64c1077b8ec39e97a88094f0fb41e
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-14 15:59:49 +0100

    Added remaining logic for auctioning off a property.

--- 32_final/src/main/java/Domain/Controller/AuctionController.java
+++ 32_final/src/main/java/Domain/Controller/AuctionController.java
@@ -26,5 +26,39 @@ public class AuctionController {
             }
         }
 
+        buyerIndex = 0;
+        boolean running = true;
+        int highestBid = field.getPrice();
+        int remainingBuyers = (buyers.length-1);
+        //Asks players if they want to buy the property
+        do {
+            if(!buyers[buyerIndex].equals(null)) {
+                String answer = guiHandler.makeButtons(buyers[buyerIndex].getName() + " vil De købe ejendommen?", "Ja", "Nej");
+                //If a players responds with no, they are nullified.
+                if (answer.equals("Nej")) {
+                    buyers[buyerIndex] = null;
+                 //If a player responds with yes, they place a bid.
+                } else {
+                    highestBid = guiHandler.getUserInt("Indtast bud på ejendom ", highestBid, 30000);
+                    buyerIndex++;
+                }
+                //If players are nullified, index is incremented
+            } else {
+                if(buyerIndex == buyers.length){
+                    buyerIndex = 0;
+                }else{
+                    buyerIndex++;
+                }
+            }
+            for(int n = 0; n < (buyers.length-1); n++){
+                if(buyers[n].equals(null)){
+                    remainingBuyers--;
+                }
+            }
+            if(remainingBuyers == 1){
+                running = false;
+            }
+        }while(running);
+
     }
 }
