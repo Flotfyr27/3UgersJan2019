05a03b8c8ad24d537fd4cb12d5235b41de6c69b2
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-14 15:00:03 +0100

    Added start to initAuction() in AuctionController.java

--- 32_final/src/main/java/Domain/Controller/AuctionController.java
+++ 32_final/src/main/java/Domain/Controller/AuctionController.java
@@ -2,12 +2,31 @@ package Domain.Controller;
 
 import Domain.GameElements.Board;
 import Domain.GameElements.Entities.Player;
+import UI.GUI.GuiHandler;
 
 public class AuctionController {
     Board board;
+    GuiHandler guiHandler;
     public AuctionController(){
         board = Board.getInstance();
+        guiHandler = GuiHandler.getInstance();
     }
 
-    public void initAuction(){}
+    public void initAuction(Player startingPlayer){
+        int nextPlayer = 0, currentPlayer = 0;
+        int indexOfStartingPlayer = 0;
+        //Get location of starting player
+        for(int n = 0; n < board.getPlayers().length; n++){
+            if(board.getPlayers()[n].equals(startingPlayer)){
+                indexOfStartingPlayer = n;
+            }
+        }
+        //Select other players for first round of bidding.
+        if(indexOfStartingPlayer == (board.getPlayers().length-1)){
+            nextPlayer = 0;
+        }else{
+            nextPlayer = ++indexOfStartingPlayer;
+        }
+
+    }
 }
