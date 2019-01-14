1d7ada18712e5e3b7571c6c40f9d7b6a3ce6e30d
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-14 14:08:43 +0100

    Added getObjectIndex()-method in Utility.java

--- 32_final/src/main/java/TechnicalServices/Utility.java
+++ 32_final/src/main/java/TechnicalServices/Utility.java
@@ -1,4 +1,13 @@
 package TechnicalServices;
 
 public class Utility {
+
+    private int getObjectIndex(Object obj, Object[] objArray){
+        for(int n = 0; n < objArray.length; n++){
+            if(objArray[n].equals(obj)){
+                return n;
+            }
+        }
+        throw new IllegalArgumentException("The object was not found in the array");
+    }
 }
