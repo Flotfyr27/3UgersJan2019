b6e2380751d3637dfdb7b7c49c83bc48c35f9356
Fredrik <s185130@student.dtu.dk>
2019-01-15 14:31:10 +0100

    Made an landOnAction for the chancecards and updating on the GUI

--- 32_final/src/main/java/UI/GUI/GuiHandler.java
+++ 32_final/src/main/java/UI/GUI/GuiHandler.java
@@ -12,7 +12,6 @@ import TechnicalServices.GameLogic.Values;
 import gui_codebehind.GUI_BoardController;
 import gui_fields.*;
 import gui_main.GUI;
-import jdk.nashorn.internal.objects.annotations.Property;
 
 import java.awt.*;
 
@@ -235,6 +234,45 @@ public class GuiHandler {
             }
 
 
+        }
+    }
+
+    /**
+     * Moves a player on the board
+     *
+     * @param player The player to move
+     * @param pArr The array of all players
+     */
+    public void updatePlayerPos(Player player, Player[] pArr) {
+        /*
+         * Move the players on the map, field by field
+         */
+        boolean carMoved = false;
+        //moves the active player up until start
+        for (int i = player.getPos() + 1; i < gui_fields.length; i++) {
+            for (int j = 0; j < guiPlayers.length; j++) {
+                if (gui_fields[i].hasCar(guiPlayers[j]) && pArr[j].getPos() != i) {
+                    gui_fields[(i + 1) % gui_fields.length].setCar(guiPlayers[j], true);
+                    carMoved = true;
+                }
+            }
+
+            gui_fields[i].setCar(findGuiPlayer(player, pArr), false);
+
+            carMoved = onCarMoved(carMoved);
+        }
+        //moves the active player after start
+        for (int i = 0; i < player.getPos(); i++) {
+            for (int j = 0; j < guiPlayers.length; j++) {
+                if (gui_fields[i].hasCar(guiPlayers[j]) && pArr[j].getPos() != i) {
+                    gui_fields[(i + 1) % gui_fields.length].setCar(guiPlayers[j], true);
+                    carMoved = true;
+                }
+            }
+
+            gui_fields[i].setCar(findGuiPlayer(player, pArr), false);
+
+            carMoved = onCarMoved(carMoved);
         }
     }
 
