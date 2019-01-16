ccc3d9a0910ff59bfceb5a0bb472588fbaa97810
Mads Møller Jensen <mads1606@gmail.com>
2019-01-16 10:00:16 +0100

    sell method implemented. doesn't crash, no further testing has been done.

--- 32_final/src/main/java/Domain/Controller/BuySellController.java
+++ 32_final/src/main/java/Domain/Controller/BuySellController.java
@@ -45,9 +45,65 @@ public class BuySellController {
 
         if (buttons.equalsIgnoreCase("Køb")) {
             buy(player);
+        } else {
+            sell(player);
         }
 
     }
+
+    private void sell(Player player) {
+        String[] possibleFields;
+        String choice;
+        PropertyField chosenField;
+
+        //counts the number of fields the player owns which have houses on them
+        int count = 0;
+        for (OwnableField field : player.getOwnedFields()){
+            if (field.getClass().equals(PropertyField.class))
+                if (((PropertyField)field).getHouses() > 0)
+                    count++;
+        }
+
+        //ads all the owned fields' names to the possibleFields array
+        possibleFields = new String[count];
+        for (int i = 0; i < player.getOwnedFields().size(); i++){
+            OwnableField field = player.getOwnedFields().get(i);
+            if (field.getClass().equals(PropertyField.class))
+                if (((PropertyField)field).getHouses() > 0)
+                    possibleFields[i] = field.getName();
+        }
+
+        //checks if any fields were found
+        if (possibleFields.length < 1){
+            guiHandler.giveMsg("Du har ingen grunde med huse på.");
+
+        } else {
+            //presents the player with their choices. String manipulation takes place for the sake of user experience.
+            addHouseCount(player, possibleFields);
+            choice = guiHandler.makeButtons("Vælg en grund du vil sælge et hus fra. Antallet af huse på feltet " +
+                    "står i parantes.", possibleFields);
+            removeHouseCount(choice);
+
+            //Finds the chosen field and assigns it to chosenField
+            chosenField = null;
+            for (OwnableField field : player.getOwnedFields()){
+                if (field.getName().equalsIgnoreCase(choice)) {
+                    chosenField = (PropertyField) field;
+                    break;
+                }
+            }
+
+            if (chosenField != null) {
+                player.getAccount().changeScore(Values.housePrice(chosenField.getHouses()));
+                chosenField.removeHouse(1);
+            } else {
+                throw new NullPointerException("the chosen field was not found");
+            }
+        }
+
+
+    }
+
 
     private void buy(Player player){
         Color[] triedColors = new Color[0];
@@ -96,6 +152,7 @@ public class BuySellController {
 
         //TODO Make check that ensures one can only build on fields that has the least amount of houses
         //TODO make check for number of houses to add hotels and cap the amount of times you can buy
+        //TODO allow the player to buy multiple buildings at a time
 
         if (ownableFields.length <= 0) {
             guiHandler.giveMsg("Du har ingen grunde du kan købe huse på.");
@@ -121,7 +178,7 @@ public class BuySellController {
     private OwnableField getChosenField(Player owner, String[] fieldNames) {
         //Select a field to trade based on user input
         if (fieldNames.length > 0) {
-            String fieldString = guiHandler.makeButtons("Vælg felt du vil bygge på", fieldNames);
+            String fieldString = guiHandler.makeButtons("Vælg felt du vil købe/sælge huse på", fieldNames);
             for (int n = 0; n < owner.getOwnedFields().size(); n++) {
                 if (fieldString.equals(fieldNames[n])) {
                     return owner.getOwnedFields().get(n);
@@ -144,5 +201,31 @@ public class BuySellController {
             temp[arr1.length + i] = arr2[i];
         }
         return temp;
+    }
+
+    /**
+     * A method for adding the number of houses on a field to the field-selection buttons
+     * @param fieldList the list of fields that is being changed
+     * @return
+     */
+    private void addHouseCount(Player player, String[] fieldList){
+        for (String fieldName : fieldList){
+            for (OwnableField field : player.getOwnedFields()){
+                if (field.getClass().equals(PropertyField.class)) {
+                    if (field.getName().equalsIgnoreCase(fieldName)) {
+                        fieldName = fieldName.concat("-(" + ((PropertyField)field).getHouses() + ")");
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Removes the house count from the string to make it usefull for serching for a property again.
+     * use it on the returned value from a button press.
+     * @param returnedString the string returned by the makeButtons() function
+     */
+    private void removeHouseCount(String returnedString){
+        returnedString = returnedString.split("-")[0];
     }
 }
