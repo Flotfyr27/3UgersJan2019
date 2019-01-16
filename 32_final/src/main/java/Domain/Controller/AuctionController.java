21079e343019c20381cea75b4aa1244d35a7df5a
Flotfyr27 <42938246+LGramme@users.noreply.github.com>
2019-01-16 14:39:31 +0100

    Added methods from code block in AuctionController.javas runCase()

--- 32_final/src/main/java/Domain/Controller/AuctionController.java
+++ 32_final/src/main/java/Domain/Controller/AuctionController.java
@@ -31,6 +31,7 @@ public class AuctionController {
         else return instance;
     }
 
+
     public void runCase(Player startingPlayer){
 
 
@@ -68,31 +69,22 @@ public class AuctionController {
                         }
                         highestBidder = buyers.get(currentPlayer);
                         currentPlayer = nextPlayer++;
-                        if((nextPlayer%buyers.size()) == 0){
-                            nextPlayer = 0;
-                        }
+                        nextPlayer = incrementNextPlayer(nextPlayer);
                     }else{
                         buyers.remove(currentPlayer);
                     }
                 }
             if(buyers.size() == 1 && !isFirstRound){
-                guiHandler.giveMsg(buyers.get(0).getName() + " har købt " + field.getName() + " for kr. " + highestBid);
-                buyers.get(0).getAccount().changeScore(-highestBid);
-                buyers.get(0).getOwnedFields().add(field);
-                field.setOwner(buyers.get(0));
+                finalizeAuction(field, highestBid, highestBidder);
                 return;
             } else if (buyers.size() == 1 && isFirstRound) {
-                Player lastBuyer = buyers.get(0);
-                String answer2 = guiHandler.makeButtons(lastBuyer.getName() + " vil De købe " + field.getName() + " for kr " + highestBid + "?", "Ja", "Nej");
+
+                String answer2 = guiHandler.makeButtons(highestBidder.getName() + " vil De købe " + field.getName() + " for kr " + highestBid + "?", "Ja", "Nej");
                 if(answer2.equals("Ja")){
-                    if(lastBuyer.getAccount().getScore() >= highestBid){
-                        lastBuyer.getAccount().changeScore(-highestBid);
-                        lastBuyer.getOwnedFields().add(field);
-                        field.setOwner(lastBuyer);
-                        guiHandler.giveMsg(lastBuyer.getName() + " har købt " + field.getName() + " for kr. " + highestBid);
+                    if(highestBidder.getAccount().getScore() >= highestBid){
+                        finalizeAuction(field, highestBid, highestBidder);
                         return;
                     }
-
                 }else{
                     return;
                 }
@@ -105,5 +97,19 @@ public class AuctionController {
 
         }while(buyers.size() >= 1);
 
+    }
+
+    private int incrementNextPlayer(int nextPlayer) {
+        if((nextPlayer%buyers.size()) == 0){
+            nextPlayer = 0;
+        }
+        return nextPlayer;
+    }
+
+    private void finalizeAuction(OwnableField field, int highestBid, Player lastBuyer) {
+        lastBuyer.getAccount().changeScore(-highestBid);
+        lastBuyer.getOwnedFields().add(field);
+        field.setOwner(lastBuyer);
+        guiHandler.giveMsg(lastBuyer.getName() + " har købt " + field.getName() + " for kr. " + highestBid);
     }
 }
