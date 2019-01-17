79b214e4afdbe1d0bce14c49d36921e92082f7b9
LGramme <42938246+LGramme@users.noreply.github.com>
2019-01-17 01:58:37 +0100

    Added method runFirstRound() in AuctionController.java

--- 32_final/src/main/java/Domain/Controller/AuctionController.java
+++ 32_final/src/main/java/Domain/Controller/AuctionController.java
@@ -14,8 +14,8 @@ public class AuctionController {
     private static AuctionController instance;
 
     private OwnableField chosenField;
-    private Player currentPlayer;
-    private int playerIndex = 0;
+    private Player currentPlayer, playerWithHighestBid = null;
+    private int playerIndex = 0, highestBid = 0;
 
 
     private AuctionController(){
@@ -44,11 +44,45 @@ public class AuctionController {
         getFieldToBeAuctioned(startingPlayer);
         //Initializes the current player
         initCurrentPlayer();
+        //Loop going through all players choosing to bid or not for the initial bidding round
+        runFirstRound();
 
     }
 
+    private void runFirstRound() {
+        boolean wantsToBuy[] = new boolean[buyers.size()];
+
+
+            for(int n = 0; n < buyers.size(); n++){
+                String answerFirstRound = guiHandler.makeButtons(currentPlayer.getName() + " vil De byde på " + chosenField.getName() + "?", "Ja", "Nej");
+                if(answerFirstRound.equals("Ja")){
+                    wantsToBuy[n] = true;
+                    if(playerWithHighestBid == null){
+                        highestBid = guiHandler.getUserInt(currentPlayer.getName() + " hvor meget ønsker De at byde på " + chosenField.getName() + "? (Mindst " +highestBid + ")", highestBid, currentPlayer.getAccount().getScore());
+                    }else if(playerWithHighestBid != null){
+                        highestBid = guiHandler.getUserInt(currentPlayer.getName() + " hvor meget ønsker De at byde på " + chosenField.getName() + "? (Højeste bud: kr. " + highestBid + " af " + playerWithHighestBid.getName(), highestBid, currentPlayer.getAccount().getScore());
+                        playerWithHighestBid = currentPlayer;
+                    }
+
+                }else if(answerFirstRound.equals("Nej")){
+                    wantsToBuy[n] = false;
+                }
+            }
+            for(int n = 0; n < wantsToBuy.length; n++){
+                if(!wantsToBuy[n]){
+                    buyers.remove(n);
+                }
+            }
+
+    }
+
+
     private void initCurrentPlayer(){
         currentPlayer = buyers.get(0);
+    }
+
+    private void updateCurrentPlayer(){
+        currentPlayer = getNextPlayer();
     }
 
     private Player getNextPlayer(){
@@ -56,12 +90,14 @@ public class AuctionController {
         if(currentPlayer == buyers.get(buyers.size())){
             nextPlayer = buyers.get(0);
         }else{
+            playerIndex++;
             nextPlayer = buyers.get(playerIndex);
         }
         return nextPlayer;
     }
     private void getFieldToBeAuctioned(Player startingPlayer) {
         chosenField = (OwnableField)board.getFields()[startingPlayer.getPos()];
+        highestBid = chosenField.getPrice();
     }
 
     private void getListOfBuyers(Player startingPlayer) {
